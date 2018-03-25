6321887f7b122844bfa35f31b84b0a4e2738cda3
ejdelrio <edwinjdelrio@gmail.com>
2018-03-24 17:29:39 -0700

    Added a simple Calculator class with two simple methods and appropriate xunit testing :D

---
+++ Calculator/Calculator/Calc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class Calc
+    {
+
+        public int AddTwo(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int SubtractTwo(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ Calculator/XUnitTestProject2/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using Calculator;
+
+namespace XUnitTestProject2
+{
+    public class CalcTest
+    {
+        Calc calc = new Calc();
+        [Fact]
+        public void TesAddTwo()
+        {
+            int output = calc.AddTwo(2, 5);
+            Assert.True(output == 7, "Expects 2 + 5 to equal 7");
+            Assert.False(output == 6, "Expects 2 + 5 to not equal 6");
+        }
+        [Fact]
+        public void TestSubtractTwo()
+        {
+            int output = calc.SubtractTwo(5, 2);
+            Assert.True(output == 3, "Expects 5 - 2 to equal 3");
+            Assert.False(output == 1, "Expects 5 - 2 to not equal 1");
+        }
+    }
+}
